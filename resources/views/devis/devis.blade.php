d676f28b16e3c19dc943cc0c4ea4d7db868f2a8d
Bastien2004 <bastienhecquet2004@gmail.com>
2026-03-19 11:15:26 +0100

    19/03/2026 --> Ajout calendrier

--- resources/views/devis/devis.blade.php
+++ resources/views/devis/devis.blade.php
@@ -8,8 +8,7 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/devisTableau.css') }}">
-    <link rel="icon" href="{{ asset('LogoHead.png') }}" type="image/png">
+    <link rel="stylesheet" href="{{ asset('css/devisTableau.css') }}?v={{ filemtime(public_path('css/devisTableau.css')) }}">    <link rel="icon" href="{{ asset('LogoHead.png') }}" type="image/png">
 </head>
 <body>
 
