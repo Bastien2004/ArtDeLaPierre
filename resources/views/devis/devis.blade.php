98ec65db8a819e7e9e6b14fc73a36bfaa6721270
Bastien2004 <bastienhecquet2004@gmail.com>
2026-04-10 14:11:55 +0200

    10/04/2026 --> Recherche datatables

--- resources/views/devis/devis.blade.php
+++ resources/views/devis/devis.blade.php
@@ -430,7 +430,9 @@
             ordering:   false,
             pageLength: 50,
             dom:        '<"top"f>rt<"bottom"lp><"clear">',
-            language:   { url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/fr-FR.json' }
+            language:   { url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/fr-FR.json' },
+            stateSave:  true,
+            stateDuration: 60 * 60 * 2, // Garde la recherche en memoire pendant 2 h
         });
 
         const urlParams = new URLSearchParams(window.location.search);
