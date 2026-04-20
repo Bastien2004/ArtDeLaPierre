b2cce862cf11fa0900adb5886e1f014adbee2df6
Bastien2004 <bastienhecquet2004@gmail.com>
2026-04-20 11:26:28 +0200

    20/04/2026 --> Fix tiime

---
+++ database/migrations/2026_04_20_112536_change_prix_in_specificites_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('specificites', function (Blueprint $table) {
+            // On définit 0 comme valeur par défaut
+            $table->decimal('prix', 10, 2)->default(0)->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('specificites', function (Blueprint $table) {
+            $table->decimal('prix', 10, 2)->default(null)->change();
+        });
+    }
+};
